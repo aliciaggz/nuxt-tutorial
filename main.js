7e338ba55576483d2415139cc9567510757c0fc8
aliciaggz <aliciagallegonz@gmail.com>
2022-04-06 10:03:26 +0200

    v-things

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const app = Vue.createApp({
     data(){
         return{
-            
-            header:'Vue is ready to party'
-            
+            cart:0,
+            header:'Vue is ready to party',
+            image: "./images/socks_green.jpg",
+            inventory:0,
+        
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1
         }
     }
 })
